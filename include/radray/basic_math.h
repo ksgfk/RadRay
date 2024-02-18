3c01bb1ec2a8dc0e481d792296bada6f71c47be3
ksgfk <1537100607@qq.com>
2024-02-18 15:48:07 +0800

    some useful

--- include/radray/basic_math.h
+++ include/radray/basic_math.h
@@ -1,23 +1,28 @@
+#include <cmath>
 #include <Eigen/Dense>
 #include <radray/types.h>
 
 namespace radray {
 
-template <typename Type, int Size>
-class Vector {
-    static_assert(always_false_v<Type>, "invalid vector");
-};
+template <typename T>
+requires(std::is_floating_point_v<T>)
+T Lerp(T a, T b, T t) noexcept {
+    return std::fma(b, t, std::fma(-a, t, a));  // a + (b - a) * t
+}
 
-template <typename Type>
-class Vector<Type, 2> {
-};
+template <typename LhsDerived, typename RhsDerived>
+typename Eigen::ScalarBinaryOpTraits<typename LhsDerived::Scalar, typename RhsDerived::Scalar>::ReturnType AbsDot(const Eigen::MatrixBase<LhsDerived>& lhs, const Eigen::MatrixBase<RhsDerived>& rhs) noexcept {
+    return std::abs(lhs.dot(rhs));
+}
 
-template <typename Type>
-class Vector<Type, 3> {
-};
-
-template <typename Type>
-class Vector<Type, 4> {
-};
+using Vector2f = Eigen::Vector<float, 2>;
+using Vector3f = Eigen::Vector<float, 3>;
+using Vector4f = Eigen::Vector<float, 4>;
+using Vector2d = Eigen::Vector<double, 2>;
+using Vector3d = Eigen::Vector<double, 3>;
+using Vector4d = Eigen::Vector<double, 4>;
+using Vector2i = Eigen::Vector<int, 2>;
+using Vector3i = Eigen::Vector<int, 3>;
+using Vector4i = Eigen::Vector<int, 4>;
 
 }  // namespace radray
