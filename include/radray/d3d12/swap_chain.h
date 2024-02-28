25708f982045642cbf0dbd68ccfe176addc86ba1
ksgfk <1537100607@qq.com>
2024-02-28 18:24:31 +0800

    swapchain

--- include/radray/d3d12/swap_chain.h
+++ include/radray/d3d12/swap_chain.h
@@ -1,7 +1,44 @@
 #pragma once
 
+#include <radray/d3d12/resource.h>
+
 namespace radray::d3d12 {
 
+class Device;
+class CommandQueue;
 
+class SwapChainRenderTarget : public Resource {
+public:
+    SwapChainRenderTarget(Device* device, ComPtr<ID3D12Resource>&& rt) noexcept;
+    ~SwapChainRenderTarget() noexcept override = default;
+    SwapChainRenderTarget(SwapChainRenderTarget&&) noexcept = default;
+    SwapChainRenderTarget(const SwapChainRenderTarget&) noexcept = delete;
+    SwapChainRenderTarget& operator=(SwapChainRenderTarget&&) noexcept = default;
+    SwapChainRenderTarget& operator=(const SwapChainRenderTarget&) noexcept = delete;
 
-}
+    ID3D12Resource* GetResource() const noexcept override;
+    D3D12_RESOURCE_STATES GetInitState() const noexcept override;
+
+    ComPtr<ID3D12Resource> rt;
+};
+
+class SwapChain {
+public:
+    SwapChain(
+        Device* device,
+        CommandQueue* queue,
+        HWND windowHandle,
+        uint32 frameCount,
+        uint32 width,
+        uint32 height,
+        bool vsync);
+
+    void Present();
+
+    ComPtr<IDXGISwapChain3> swapChain;
+    std::vector<SwapChainRenderTarget> renderTargets;
+    uint32 backBufferIndex;
+    bool isVsync;
+};
+
+}  // namespace radray::d3d12
---
+++ src/d3d12/swap_chain.cpp
@@ -0,0 +1,69 @@
+#include <radray/d3d12/swap_chain.h>
+
+#include <radray/d3d12/device.h>
+#include <radray/d3d12/command_queue.h>
+
+namespace radray::d3d12 {
+
+SwapChainRenderTarget::SwapChainRenderTarget(Device* device, ComPtr<ID3D12Resource>&& rt) noexcept : Resource(device), rt(std::move(rt)) {}
+
+ID3D12Resource* SwapChainRenderTarget::GetResource() const noexcept {
+    return rt.Get();
+}
+
+D3D12_RESOURCE_STATES SwapChainRenderTarget::GetInitState() const noexcept {
+    return D3D12_RESOURCE_STATE_PRESENT;
+}
+
+SwapChain::SwapChain(
+    Device* device,
+    CommandQueue* queue,
+    HWND windowHandle,
+    uint32 frameCount,
+    uint32 width,
+    uint32 height,
+    bool vsync)
+    : backBufferIndex(0),
+      isVsync(vsync) {
+    bool canTearing = true;
+    ThrowIfFailed(device->dxgiFactory->CheckFeatureSupport(DXGI_FEATURE_PRESENT_ALLOW_TEARING, &canTearing, sizeof(decltype(canTearing))));
+    if (!canTearing) {
+        isVsync = true;
+    }
+    if (!vsync && isVsync) {
+        RADRAY_LOG_WARN("DXGI check tearing feature doesn't support. vsync-off cannot work");
+    }
+    DXGI_SWAP_CHAIN_DESC1 desc{
+        .Width = width,
+        .Height = height,
+        .Format = DXGI_FORMAT_R8G8B8A8_UNORM,
+        .SampleDesc = {.Count = 1},
+        .BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT,
+        .BufferCount = frameCount,
+        .SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD,
+        .Flags = canTearing ? DXGI_SWAP_CHAIN_FLAG_ALLOW_TEARING : 0u};
+    {
+        ComPtr<IDXGISwapChain1> tempSc;
+        ThrowIfFailed(device->dxgiFactory->CreateSwapChainForHwnd(
+            queue->queue.Get(),
+            windowHandle,
+            &desc,
+            nullptr,
+            nullptr,
+            tempSc.GetAddressOf()));
+        tempSc.As(&swapChain);
+    }
+    renderTargets.reserve(desc.BufferCount);
+    for (uint32_t n = 0; n < desc.BufferCount; n++) {
+        ComPtr<ID3D12Resource> rt;
+        ThrowIfFailed(swapChain->GetBuffer(n, IID_PPV_ARGS(rt.GetAddressOf())));
+        renderTargets.emplace_back(SwapChainRenderTarget{device, std::move(rt)});
+    }
+    backBufferIndex = swapChain->GetCurrentBackBufferIndex();
+}
+
+void SwapChain::Present() {
+    swapChain->Present(isVsync, isVsync ? 0 : DXGI_SWAP_CHAIN_FLAG_ALLOW_TEARING);
+}
+
+}  // namespace radray::d3d12
