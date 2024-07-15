45960b387ff2251294dd5396a61df463190257f2
xuesen.xiao <1537100607@qq.com>
2024-07-15 16:28:51 +0800

    simple tri buffering

--- src/test/test_init_rhi/main.cpp
+++ src/test/test_init_rhi/main.cpp
@@ -8,6 +8,8 @@
 
 using namespace radray;
 
+constexpr int FRAME_COUNT = 3;
+
 int main() {
     {
         rhi::SupportApiArray api{};
@@ -35,34 +37,60 @@ int main() {
         auto sch = device->CreateSwapChain(
             {glfw.GetNativeHandle(),
              1280, 720,
-             3,
+             FRAME_COUNT,
              true},
             cmdQueue.Handle);
         uint64_t fenceValue = 0;
+        std::array<uint64_t, FRAME_COUNT> frameRes{};
+        for (uint64_t& i : frameRes) {
+            i = 0;
+        }
+        uint64_t frameValue = 0;
         auto last = std::chrono::system_clock::now();
+        float fpsTime = 0;
+        uint32_t fpsCount = 0;
         std::array<float, 4> clearColor{0.5f, 0.2f, 0.1f, 1.0f};
         while (!glfw.ShouldClose()) {
             window::GlobalPollEventsGlfw();
-            auto now = std::chrono::system_clock::now();
-            float delta = std::chrono::duration_cast<std::chrono::milliseconds>(now - last).count() / 1000.0f;
-            for (auto&& i : clearColor) {
-                i += delta * 0.25f;
-                i -= std::floor(i);
+            {  // update
+                auto now = std::chrono::system_clock::now();
+                float delta = std::chrono::duration_cast<std::chrono::milliseconds>(now - last).count() / 1000.0f;
+                for (auto&& i : clearColor) {
+                    i += delta * 0.25f;
+                    i -= std::floor(i);
+                }
+                clearColor[3] = 1.0f;
+                fpsTime += delta;
+                fpsCount++;
+                if (fpsTime >= 1) {
+                    RADRAY_INFO_LOG("fps {}", fpsCount / fpsTime);
+                    fpsTime = 0;
+                    fpsCount = 0;
+                }
+                last = now;
             }
-            clearColor[3] = 1.0f;
-            last = now;
-            if (fenceValue > 0) {
-                device->Synchronize(fence, fenceValue);
+            {  // render
+                auto renderFrame = frameValue;
+                auto showFrame = (renderFrame + 1) % FRAME_COUNT;
+                auto idleFrame = (showFrame + 1) % FRAME_COUNT;
+                if (frameRes[idleFrame] > 0) {
+                    device->Synchronize(fence, frameRes[idleFrame]);
+                }
+                device->StartFrame(cmdQueue, sch);
+                {
+                    rhi::CommandList list{};
+                    list.Add(rhi::ClearRenderTargetCommand{sch, clearColor});
+                    device->DispatchCommand(cmdQueue, std::move(list));
+                }
+                frameRes[idleFrame] = ++fenceValue;
+                device->Signal(fence, cmdQueue, frameRes[idleFrame]);
+                device->FinishFrame(cmdQueue, sch);
+                frameValue = (frameValue + 1) % FRAME_COUNT;
             }
-            device->StartFrame(cmdQueue, sch);
-            {
-                rhi::CommandList list{};
-                list.Add(rhi::ClearRenderTargetCommand{sch, clearColor});
-                device->DispatchCommand(cmdQueue, std::move(list));
-            }
-            device->Signal(fence, cmdQueue, ++fenceValue);
-            device->FinishFrame(cmdQueue, sch);
             std::this_thread::yield();
+        }
+        for (uint64_t i : frameRes) {
+            device->Synchronize(fence, i);
         }
         device->DestroySwapChain(sch);
         device->DestroyFence(fence);
