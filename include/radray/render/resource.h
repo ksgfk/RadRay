1d716d89d5fbf7e3fd7fff6a7abf00ff419bcf15
ksgfk <1537100607@qq.com>
2024-12-05 23:48:51 +0800

    resource

--- include/radray/render/resource.h
+++ include/radray/render/resource.h
@@ -7,6 +7,24 @@ namespace radray::render {
 class Resource : public RenderBase {
 public:
     ~Resource() noexcept override = default;
+
+    virtual ResourceType GetType() const noexcept = 0;
+    virtual ResourceState GetInitState() const noexcept = 0;
+};
+
+class Texture : public Resource {
+public:
+    ~Texture() noexcept override = default;
+};
+
+class RenderTarget : public Texture {
+public:
+    ~RenderTarget() noexcept override = default;
+};
+
+class Buffer : public Resource {
+public:
+    ~Buffer() noexcept override = default;
 };
 
 }  // namespace radray::render
