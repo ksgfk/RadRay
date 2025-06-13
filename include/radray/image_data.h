1630ecbc5d6c7eb96178e2e5765b1375e24ed388
ksgfk <1537100607@qq.com>
2025-06-13 22:34:13 +0800

    flip y no memcpy

--- include/radray/image_data.h
+++ include/radray/image_data.h
@@ -11,36 +11,43 @@
 namespace radray {
 
 enum class ImageFormat {
-    R8_BYTE,       // 1
-    R16_USHORT,    // 2
-    R16_HALF,      // 2
-    R32_FLOAT,     // 4
+    R8_BYTE,     // 1
+    R16_USHORT,  // 2
+    R16_HALF,    // 2
+    R32_FLOAT,   // 4
 
-    RG8_BYTE,      // 2
-    RG16_USHORT,   // 4
-    RG16_HALF,     // 4
-    RG32_FLOAT,    // 8
+    RG8_BYTE,     // 2
+    RG16_USHORT,  // 4
+    RG16_HALF,    // 4
+    RG32_FLOAT,   // 8
 
-    RGB32_FLOAT,   // 12
+    RGB32_FLOAT,  // 12
 
-    RGBA8_BYTE,    // 4
-    RGBA16_USHORT, // 8
-    RGBA16_HALF,   // 8
-    RGBA32_FLOAT,  // 16
+    RGBA8_BYTE,     // 4
+    RGBA16_USHORT,  // 8
+    RGBA16_HALF,    // 8
+    RGBA32_FLOAT,   // 16
 
-    RGB8_BYTE,     // 3
-    RGB16_USHORT   // 6
+    RGB8_BYTE,    // 3
+    RGB16_USHORT  // 6
 };
 
 size_t GetImageFormatSize(ImageFormat format) noexcept;
 
 class ImageData {
 public:
+    ImageData() noexcept = default;
+    ImageData(const ImageData&) noexcept;
+    ImageData(ImageData&&) noexcept;
+    ImageData& operator=(const ImageData&) noexcept;
+    ImageData& operator=(ImageData&&) noexcept;
+    ~ImageData() noexcept = default;
+
     size_t GetSize() const noexcept;
     std::span<const byte> GetSpan() const noexcept;
 
     ImageData RGB8ToRGBA8(uint8_t alpha) const noexcept;
-    ImageData FlipY() const noexcept;
+    void FlipY() noexcept;
 
     radray::unique_ptr<byte[]> Data;
     uint32_t Width;
